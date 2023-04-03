6de64a263778da19d79455096ab1f35a3b802aaa
Erick Borda <erickborda96@gmail.com>
2023-04-03 08:25:50 -0400

    Priority Queue training test

---
+++ app/src/test/java/dsa/training/brocode/queues/PriorityQueueExampleTest.java
@@ -0,0 +1,26 @@
+package dsa.training.brocode.queues;
+
+import org.junit.jupiter.api.Test;
+
+public class PriorityQueueExampleTest {
+    @Test
+    void testFirstExample() {
+        PriorityQueueExample.firstExample();
+    }
+
+    @Test
+    void testReverseOrderExample() {
+        PriorityQueueExample.reverseOrderExample();
+    }
+
+    @Test
+    void testStringOrderExample() {
+        PriorityQueueExample.stringOrderExample();
+    }
+
+    @Test
+    void testStringReverseOrderExample() {
+        PriorityQueueExample.stringReverseOrderExample();
+    }
+
+}
